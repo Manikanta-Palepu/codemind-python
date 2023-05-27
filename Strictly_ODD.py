6ece311e1b7c1cf0ca9c8ad4150596bfa91e91ca
Manikanta Palepu <98867138+Manikanta-Palepu@users.noreply.github.com>
2023-05-27 11:55:27 +0530

    n = int(input()) lst = list(map(int,input().split())) a = 0 for i in range(len(lst)):     if lst[i]%2==0 and i%2!=0:          a = 1          break if a == 1:     print('False') else:     print('True')

---
+++ Strictly_ODD.py
@@ -0,0 +1,11 @@
+n = int(input())
+lst = list(map(int,input().split()))
+a = 0
+for i in range(len(lst)):
+    if lst[i]%2==0 and i%2!=0:
+         a = 1
+         break
+if a == 1:
+    print('False')
+else:
+    print('True')
