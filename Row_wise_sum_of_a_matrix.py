e88b56f813138f364fc13cefc6cf2a84f468c81a
Manikanta Palepu <98867138+Manikanta-Palepu@users.noreply.github.com>
2023-05-29 13:46:24 +0530

    r,c = map(int,input().split()) arr = [list(map(int,input().split())) for i in range (r)] for i in arr:     print(sum(i),end=' ')

--- Row_wise_sum_of_a_matrix.py
+++ Row_wise_sum_of_a_matrix.py
@@ -1,5 +1,4 @@
 r,c = map(int,input().split())
-arr = [list(map(int,input().split())) for i in range(r)]
-s = 0
+arr = [list(map(int,input().split())) for i in range (r)]
 for i in arr:
-    print(sum(i),end =' ')
+    print(sum(i),end=' ')
