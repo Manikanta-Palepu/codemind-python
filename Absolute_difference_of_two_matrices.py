108d3772fb70c90cb965daace2c6575216388e73
Manikanta Palepu <98867138+Manikanta-Palepu@users.noreply.github.com>
2023-05-29 10:25:33 +0530

    n = int(input()) mat1 = [] mat2 = [] for i in range(n):     x = list(map(int,input().split()))     mat1.append(x) for j in range(n):     y = list(map(int,input().split()))     mat2.append(y) mat3 = [] for i in range(n):     a = []     for j in range(n):         a.append(abs(mat1[i][j]-mat2[i][j]))     mat3.append(a) for i in mat3:     print(*i)

--- Absolute_difference_of_two_matrices.py
+++ Absolute_difference_of_two_matrices.py
@@ -7,12 +7,11 @@ for i in range(n):
 for j in range(n):
     y = list(map(int,input().split()))
     mat2.append(y)
-mat3=[]
+mat3 = []
 for i in range(n):
     a = []
     for j in range(n):
-        a.append(abs(mat1[i][j] - mat2[i][j]))
+        a.append(abs(mat1[i][j]-mat2[i][j]))
     mat3.append(a)
 for i in mat3:
     print(*i)
-    
