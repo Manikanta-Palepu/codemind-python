74bfef303b5c54b4f8691ab5bf2984daccdfe3d5
Manikanta Palepu <98867138+Manikanta-Palepu@users.noreply.github.com>
2023-05-31 10:05:42 +0530

    n = input() l = [] a = ("a","e","i","o","u") for i in n:     if i not in l and i in a:         l.append(i) if len(l) == 5:     print('0') else:     for i in a:         if i not in l:             print(i, end = ' ')

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -5,9 +5,8 @@ for i in n:
     if i not in l and i in a:
         l.append(i)
 if len(l) == 5:
-    print("0")
-    
+    print('0')
 else:
     for i in a:
         if i not in l:
-            print(i , end = ' ')
+            print(i, end = ' ')
