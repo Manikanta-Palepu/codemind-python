7c776e5b756b2f385d0ee7f3578be50ee0a6e583
Manikanta Palepu <98867138+Manikanta-Palepu@users.noreply.github.com>
2023-06-06 09:30:58 +0530

    n = int(input()) l = list(map(int,input().split())) c = 0 for i in range(0,n):     f = 0     for j in range(2,l[i]):         if l[i]%j == 0:             f += 1     if l[i]>1 and l[i]!=0 and f==0:         c+=1 print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,11 @@
+n = int(input())
+l = list(map(int,input().split()))
+c = 0
+for i in range(0,n):
+    f = 0
+    for j in range(2,l[i]):
+        if l[i]%j == 0:
+            f += 1
+    if l[i]>1 and l[i]!=0 and f==0:
+        c+=1
+print(c)
